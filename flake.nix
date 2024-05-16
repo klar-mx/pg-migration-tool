0fd604c491d039f57b6f7660b3f4d93de74fa99d
Andriy Mykhaylyk <andriy.mykhaylyk@klar.mx>
2024-05-16 08:11:04 +0200

    move to correct python version

--- flake.nix
+++ flake.nix
@@ -5,13 +5,14 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      pythonPackage = pkgs.python311;
+      pythonPackage = pkgs.python312;
       myPython = (pythonPackage.withPackages (ps: with ps; [ pip ]));
     in {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           myPython
           (poetry.override { python3 = pythonPackage; })
+          postgresql
         ];
       };
     };
