0ee5aa2f0ff9b8bfb390abaf39ae23e71554a8a4
Andriy Mykhaylyk <andriy.mykhaylyk@klar.mx>
2024-05-16 11:22:14 +0200

    Poetry and friends (#9)

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      pythonPackage = pkgs.python312;
+      myPython = (pythonPackage.withPackages (ps: with ps; [ pip ]));
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          myPython
+          (poetry.override { python3 = pythonPackage; })
+          postgresql
+        ];
+      };
+    };
+}
