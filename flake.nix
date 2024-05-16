27e85794ab45475f08dc7864b2d16d5d715d06ec
Andriy Mykhaylyk <andriy.mykhaylyk@klar.mx>
2024-05-16 07:43:27 +0200

    migrate to Poetry

--- flake.nix
+++ flake.nix
@@ -5,8 +5,14 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      pythonPackage = pkgs.python311;
+      myPython = (pythonPackage.withPackages (ps: with ps; [ pip ]));
     in {
-      devShells.${system}.default =
-        pkgs.mkShell { buildInputs = with pkgs; [ python312 pipenv ]; };
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          myPython
+          (poetry.override { python3 = pythonPackage; })
+        ];
+      };
     };
 }
